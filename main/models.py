c931db81663cbccf2cfe8d14b78d8a25991a6108
Atabek04 <otabek.shadimatov@gmail.com>
2022-08-06 01:08:41 +0600

    fixing views.py 505

--- main/models.py
+++ main/models.py
@@ -1,89 +1,95 @@
 from django.db import models
 
-class Category(models.Model):
-	title = models.CharField(max_length=300)
-	logo = models.ImageField(upload_to='uploads')
-	is_main = models.BooleanField()
 
-	def __str__(self):
-		return self.title
+class Category(models.Model):
+    title = models.CharField(max_length=300)
+    logo = models.ImageField(upload_to='uploads')
+    is_main = models.BooleanField()
+    
+    def __str__(self):
+        return self.title
+
 
 class Size(models.Model):
-	title = models.CharField(max_length=300)
+    title = models.CharField(max_length=300)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class Color(models.Model):
-	title = models.CharField(max_length=300)
-	code = models.CharField(max_length=300)
+    title = models.CharField(max_length=300)
+    code = models.CharField(max_length=300)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
 
 class Product(models.Model):
-	title = models.CharField(max_length=300)
-	views = models.IntegerField(default=0)
-	rating = models.FloatField(default=0.0)
-	old_price = models.IntegerField(default=0)
-	new_price = models.IntegerField(default=0)
-	size = models.ForeignKey('Size', on_delete=models.CASCADE, null = True)
-	color = models.ForeignKey('Color', on_delete=models.CASCADE, null = True)
-	category = models.ForeignKey(Category, on_delete=models.CASCADE)
-	logo = models.ImageField(upload_to='uploads')
-	discount = models.IntegerField(default=0)
-	is_new = models.BooleanField()
-	is_best_seller = models.BooleanField()
-	short_description = models.TextField(max_length=500)
-	description = models.TextField(max_length=1000, null = True)
-	details = models.TextField(max_length=1000, null = True)
+    title = models.CharField(max_length=300)
+    views = models.IntegerField(default=0)
+    rating = models.FloatField(default=0.0)
+    old_price = models.IntegerField(default=0)
+    new_price = models.IntegerField(default=0)
+    size = models.ForeignKey('Size', on_delete=models.CASCADE, null=True)
+    color = models.ForeignKey('Color', on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    logo = models.ImageField(upload_to='uploads')
+    discount = models.IntegerField(default=0)
+    is_new = models.BooleanField()
+    is_best_seller = models.BooleanField()
+    short_description = models.TextField(max_length=500)
+    description = models.TextField(max_length=1000, null=True)
+    details = models.TextField(max_length=1000, null=True)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
 
 class ProductImage(models.Model):
-	product = models.ForeignKey('Product', on_delete=models.CASCADE)
-	title = models.CharField(max_length=300)
-	image = models.ImageField(upload_to='uploads')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='uploads')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
 
 class CommentItem(models.Model):
-	product = models.ForeignKey('Product', on_delete=models.CASCADE)
-	text = models.TextField(max_length=500)
-	author_name = models.CharField(max_length=300)
-	author_email = models.CharField(max_length=300)
-	rating = models.FloatField(default=0.0)
-	pub_date = models.DateField()
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    text = models.TextField(max_length=500)
+    author_name = models.CharField(max_length=300)
+    author_email = models.CharField(max_length=300)
+    rating = models.FloatField(default=0.0)
+    pub_date = models.DateField()
 
-	def __str__(self):
-		return self.author_name
+    def __str__(self):
+        return self.author_name
 
 
 class Cart(models.Model):
-	title = models.CharField(max_length=300, blank=True, default='')
-	person = models.CharField(max_length=200, blank=True, default='')
-	phone = models.CharField(max_length=200, blank=True, default='')
-	adress = models.CharField(max_length=200, blank=True, default='')
-	payed = models.IntegerField(default=0)
-	status = models.IntegerField(default=0)
-	session_id = models.CharField(max_length=300, blank=True, default='')
-	amount = models.IntegerField(default=0)
-	discount = models.IntegerField(default=0)
-	orig_price = models.IntegerField(default=0)
-	price = models.IntegerField(default=0)
+    title = models.CharField(max_length=300, blank=True, default='')
+    person = models.CharField(max_length=200, blank=True, default='')
+    phone = models.CharField(max_length=200, blank=True, default='')
+    adress = models.CharField(max_length=200, blank=True, default='')
+    payed = models.IntegerField(default=0)
+    status = models.IntegerField(default=0)
+    session_id = models.CharField(max_length=300, blank=True, default='')
+    amount = models.IntegerField(default=0)
+    discount = models.IntegerField(default=0)
+    orig_price = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.session_id
+    def __str__(self):
+        return self.session_id
+
 
 class CartItem(models.Model):
-	product = models.ForeignKey('Product', on_delete= models.CASCADE)
-	amount = models.IntegerField(default=0)
-	product_price = models.IntegerField(default=0)
-	cart  = models.ForeignKey('Cart', on_delete = models.CASCADE)
-	status = models.IntegerField(default=0)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    amount = models.IntegerField(default=0)
+    product_price = models.IntegerField(default=0)
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    status = models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.product.title
+    def __str__(self):
+        return self.product.title
--- main/views.py
+++ main/views.py
@@ -1,226 +1,236 @@
+from math import prod
 from django.shortcuts import render
 from main.models import *
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
 
 
 def indexHandler(request):
-	categories = Category.objects.all()
-	open_carts = Cart.objects.filter(session_id = request.session.session_key).filter(status=0)
+    categories = Category.objects.all()
+    open_carts = Cart.objects.filter(session_id=request.session.session_key).filter(status=0)
 
-	cart = {
-		'info': {},
-		'product_count':0,
-		'products': [],
-		'price':0
-	}
+    cart = {
+        'info': {},
+        'product_count': 0,
+        'products': [],
+        'price': 0
+    }
 
-	if open_carts:
-		open_carts = open_carts[0]
-		cart_items = CartItem.objects.filter(cart__id = open_carts.id).filter(status=0)
-		cart['info'] = open_carts
-		if cart_items:
-			cart['product_count'] = len(cart_items)
-			cart['products'] = cart_items
+    if open_carts:
+        open_carts = open_carts[0]
+        cart_items = CartItem.objects.filter(cart__id=open_carts.id).filter(status=0)
+        cart['info'] = open_carts
+        if cart_items:
+            cart['product_count'] = len(cart_items)
+            cart['products'] = cart_items
 
-
-	context = {
-		'categories':categories,
-		'cart':cart,
-	}	
-	return render(request, 'index.html', context)
+    context = {
+        'categories': categories,
+        'cart': cart,
+    }
+    return render(request, 'index.html', context)
 
 
 def productsHandler(request):
-	categories = Category.objects.all()
-	active_category = None
+    categories = Category.objects.all()
+    active_category = None
 
-	search_value = request.GET.get('q', '')
-	category_id = int(request.GET.get('category_id', 0))
-	print('='*100)
-	print(category_id)
+    search_value = request.GET.get('q', '')
+    category_id = int(request.GET.get('category_id', 0))
 
-	if category_id:
-		if search_value:
-			products = Product.objects.filter(category__id=category_id).filter(title__contains = search_value)
-		else:
-			products = Product.objects.filter(category__id=category_id)
+    if category_id:
+        if search_value:
+            products = Product.objects.filter(category__id=category_id).filter(title__contains=search_value)
+        else:
+            products = Product.objects.filter(category__id=category_id)
 
-		active_category = Category.objects.get(id=category_id)
-	else:
-		if search_value:
-			products = Product.objects.filter(title__contains = search_value)
-		else:
-			products = Product.objects.all()
+        active_category = Category.objects.get(id=category_id)
+    else:
+        if search_value:
+            products = Product.objects.filter(title__contains=search_value)
+        else:
+            products = Product.objects.all()
 
-	cart = {
-		'info': {},
-		'product_count':0,
-		'products': [],
-		'price':0
-	}
-	open_carts = Cart.objects.filter(session_id = request.session.session_key).filter(status=0)
-	if open_carts:
-		open_carts = open_carts[0]
-		cart_items = CartItem.objects.filter(cart__id = open_carts.id).filter(status=0)
-		cart['info'] = open_carts
-		if cart_items:
-			cart['product_count'] = len(cart_items)
-			cart['products'] = cart_items
+    cart = {
+        'info': {},
+        'product_count': 0,
+        'products': [],
+        'price': 0
+    }
+    open_carts = Cart.objects.filter(session_id=request.session.session_key).filter(status=0)
+    if open_carts:
+        open_carts = open_carts[0]
+        cart_items = CartItem.objects.filter(cart__id=open_carts.id).filter(status=0)
+        cart['info'] = open_carts
+        if cart_items:
+            cart['product_count'] = len(cart_items)
+            cart['products'] = cart_items
 
-
-
-	results = len(products)
-	context = {
-		'products':products,
-		'categories':categories,
-		'active_category':active_category,
-		'results':results,
-		'search_value':search_value,
-		'cart':cart
-	}
-	return render(request, 'products.html', context)
+    results = len(products)
+    context = {
+        'products': products,
+        'categories': categories,
+        'active_category': active_category,
+        'results': results,
+        'search_value': search_value,
+        'cart': cart
+    }
+    return render(request, 'products.html', context)
 
 
 def productHandler(request, product_id):
-	product = Product.objects.filter(id=int(product_id))
-	sizes = Size.objects.all()
-	colors = Color.objects.all()
-	prod_img = ProductImage.objects.filter(product__id=product_id)
-	comment_items = CommentItem.objects.filter(product__id=product_id)
+    product = Product.objects.filter(id=int(product_id))
+    sizes = Size.objects.all()
+    colors = Color.objects.all()
+    prod_img = ProductImage.objects.filter(product__id=product_id)
+    comment_items = CommentItem.objects.filter(product__id=product_id)
 
+    comments_len = 0
+    comments_summa = 0
+    rating_1 = 0
+    rating_2 = 0
+    rating_3 = 0
+    rating_4 = 0
+    rating_5 = 0
 
-	comments_len = 0
-	comments_summa = 0
-	rating_1 = 0
-	rating_2 = 0
-	rating_3 = 0
-	rating_4 = 0
-	rating_5 = 0
+    rating_1_progress = 0
+    rating_2_progress = 0
+    rating_3_progress = 0
+    rating_4_progress = 0
+    rating_5_progress = 0
+    arith_mean = 0
 
-	rating_1_progress = 0
-	rating_2_progress = 0
-	rating_3_progress = 0
-	rating_4_progress = 0
-	rating_5_progress = 0
-	arith_mean = 0
+    for comment in comment_items:
+        comments_len += 1
+        comments_summa += comment.rating
+        if comment.rating == 1:
+            rating_1 += 1
+        elif comment.rating == 2:
+            rating_2 += 1
+        elif comment.rating == 3:
+            rating_3 += 1
+        elif comment.rating == 4:
+            rating_4 += 1
+        elif comment.rating == 5:
+            rating_5 += 1
 
-	for comment in comment_items:
-		comments_len += 1
-		comments_summa += comment.rating
-		if comment.rating == 1:
-			rating_1 += 1
-		elif comment.rating == 2:
-			rating_2 += 1
-		elif comment.rating == 3:
-			rating_3 += 1
-		elif comment.rating == 4:
-			rating_4 += 1
-		elif comment.rating == 5:
-			rating_5 += 1
+    if comments_len > 0:
+        arith_mean = int(comments_summa / comments_len * 10) / 10
+        rating_1_progress = int((rating_1 / comments_len) * 100)
+        rating_2_progress = int((rating_2 / comments_len) * 100)
+        rating_3_progress = int((rating_3 / comments_len) * 100)
+        rating_4_progress = int((rating_4 / comments_len) * 100)
+        rating_5_progress = int((rating_5 / comments_len) * 100)
 
-	if comments_len > 0:
-		arith_mean = int(comments_summa / comments_len *10)/10
-		rating_1_progress = int((rating_1/comments_len)*100)
-		rating_2_progress = int((rating_2/comments_len)*100)
-		rating_3_progress = int((rating_3/comments_len)*100)
-		rating_4_progress = int((rating_4/comments_len)*100)
-		rating_5_progress = int((rating_5/comments_len)*100)
+    cart = {
+        'info': {},
+        'product_count': 0,
+        'products': [],
+        'price': 0
+    }
+    open_carts = Cart.objects.filter(session_id=request.session.session_key).filter(status=0)
+    if open_carts:
+        open_carts = open_carts[0]
+        cart_items = CartItem.objects.filter(cart__id=open_carts.id).filter(status=0)
+        cart['info'] = open_carts
+        if cart_items:
+            cart['product_count'] = len(cart_items)
+            cart['products'] = cart_items
 
-	cart = {
-		'info': {},
-		'product_count':0,
-		'products': [],
-		'price':0
-	}
-	open_carts = Cart.objects.filter(session_id = request.session.session_key).filter(status=0)
-	if open_carts:
-		open_carts = open_carts[0]
-		cart_items = CartItem.objects.filter(cart__id = open_carts.id).filter(status=0)
-		cart['info'] = open_carts
-		if cart_items:
-			cart['product_count'] = len(cart_items)
-			cart['products'] = cart_items
+    context = {
+        'product': product,
+        'sizes': sizes,
+        'colors': colors,
+        'prod_img': prod_img,
+        'comment_items': comment_items,
+        'rating_1': rating_1,
+        "rating_2": rating_2,
+        "rating_3": rating_3,
+        "rating_4": rating_4,
+        "rating_5": rating_5,
+        "arith_mean": arith_mean,
+        "rating_1_progress": rating_1_progress,
+        "rating_2_progress": rating_2_progress,
+        "rating_3_progress": rating_3_progress,
+        "rating_4_progress": rating_4_progress,
+        "rating_5_progress": rating_5_progress,
+        'cart': cart
 
+    }
+    return render(request, 'product.html', context)
 
-	context = {
-		'product':product,
-		'sizes':sizes,
-		'colors':colors,
-		'prod_img':prod_img,
-		'comment_items':comment_items,
-		'rating_1':rating_1,
-		"rating_2":rating_2,
-		"rating_3":rating_3,
-		"rating_4":rating_4,
-		"rating_5":rating_5,
-		"arith_mean":arith_mean,
-		"rating_1_progress":rating_1_progress,
-		"rating_2_progress":rating_2_progress,
-		"rating_3_progress":rating_3_progress,
-		"rating_4_progress":rating_4_progress,
-		"rating_5_progress":rating_5_progress,
-		'cart':cart
-
-	}
-	return render(request, 'product.html', context)
-
-
+@csrf_exempt
 def cartHandler(request):
-	if request.method == 'POST':
-		action = request.POST.get('action','')
-		if action == 'add_to_cart':
-			new_cart = None
-			product_id = int(request.POST.get('product_id', 0))
-			amount = int(request.POST.get('amount', 0))
-			open_carts = Cart.objects.filter(session_id = request.session.session_key).filter(status=0)
-			if not open_carts:
-				new_cart = Cart()
-				new_cart.session_id = request.session.session_key
-				new_cart.save()
-			else:
-				new_cart = open_carts[0]
+    if request.method == 'POST':
+        action = request.POST.get('action', '')
+        if action == 'add_to_cart':
+        
+            new_cart = None
 
-			cart_items = CartItem.objects.filter(cart__id = new_cart.id).filter(product__id=product_id).filter(status=0)
-			if cart_items:
-				new_cart_item = cart_items[0]
-				new_cart_item.amount += amount
-				new_cart_item.save()
-			else:
-				new_cart_item = CartItem()
-				new_cart_item.product = Product.objects.get(id=product_id)
-				new_cart_item.cart = Cart.objects.get(id=new_cart.id)
-				new_cart_item.amount = amount
-				new_cart_item.product_price = new_cart_item.product.new_price
-				new_cart_item.save()
+            # product_id = int(request.GET.get('product_id', 0))
+            if request.POST.get('product_id') !='':
+                product_id = int(request.GET.get('product_id'))
+            else:
+                product_id = 1
+            
+            print('*'*100)
+            print(type(request.POST.get('product_id')))
+            print(product_id)
 
-		elif action == 'remove_from_cart':
-			product_id = int(request.POST.get('product_id', 0))
-			open_carts = Cart.objects.filter(session_id = request.session.session_key).filter(status=0)
-			if open_carts:
-				new_cart = open_carts[0]
-			elif new_cart:
-				cart_items = CartItem.objects.filter(cart__id = new_cart.id).filter(product__id = product_id).filter(status=0)
-				for ci in cart_items:
-					ci.status = -1
-					ci.save()
+            amount = int(request.POST.get('amount', 0))
+            open_carts = Cart.objects.filter(session_id=request.session.session_key).filter(status=0)
+            if not open_carts:
+                new_cart = Cart()
+                new_cart.session_id = request.session.session_key
+                new_cart.save()
+            else:
+                new_cart = open_carts[0]
 
-		if action in ['add_to_cart', 'remove_from_cart']:
-			open_carts = Cart.objects.filter(session_id = request.session.session_key).filter(status=0)
-			if open_carts:
-				new_cart = open_carts[0]
-				cart_items = CartItem.objects.filter(cart__id = new_cart.id).filter(product__id = product_id).filter(status=0)
-				all_summ = 0
-				items_count = 0
-				for ci in cart_items:
-					all_summ += ci.amount * ci.product_price
-					items_count +=ci.amount
-				new_cart.orig_price = all_summ
-				new_cart.price = all_summ
-				new_cart.amount = items_count
-				new_cart.save()
- 
+            cart_items = CartItem.objects.filter(cart__id=new_cart.id).filter(product__id=product_id).filter(status=0)
+            if cart_items:
+                new_cart_item = cart_items[0]
+                new_cart_item.amount += amount
+                new_cart_item.save()
+            else:
+                new_cart_item = CartItem()
+                new_cart_item.product = Product.objects.get(id=product_id)
+                new_cart_item.cart = Cart.objects.get(id=new_cart.id)
+                new_cart_item.amount = amount
+                new_cart_item.product_price = new_cart_item.product.new_price
+                new_cart_item.save()
 
-		return JsonResponse({'success':True, 'success':True})
-	return render(request, 'cart.html', {
-		'cart':cart 
-		})
+        elif action == 'remove_from_cart':
+            product_id = int(request.POST.get('product_id', 0))
+            open_carts = Cart.objects.filter(session_id=request.session.session_key).filter(status=0)
+            if open_carts:
+                new_cart = open_carts[0]
+            elif new_cart:
+                cart_items = CartItem.objects.filter(cart__id=new_cart.id).filter(product__id=product_id).filter(
+                    status=0)
+                for ci in cart_items:
+                    ci.status = -1
+                    ci.save()
+
+        if action in ['add_to_cart', 'remove_from_cart']:
+            open_carts = Cart.objects.filter(session_id=request.session.session_key).filter(status=0)
+            if open_carts:
+                new_cart = open_carts[0]
+                cart_items = CartItem.objects.filter(cart__id=new_cart.id).filter(product__id=product_id).filter(
+                    status=0)
+                all_summ = 0
+                items_count = 0
+                for ci in cart_items:
+                    all_summ += ci.amount * ci.product_price
+                    items_count += ci.amount
+                new_cart.orig_price = all_summ
+                new_cart.price = all_summ
+                new_cart.amount = items_count
+                new_cart.save()
+    
+
+    return JsonResponse({'success': True, 'success': True})
+    return render(request, 'cart.html', {
+        'cart': cart
+    })
